022eb2d6b4db091c0432f2781f2876659952560f
Linda <oneill.linda21@gmail.com>
2018-03-26 21:00:26 -0500

    add remove_entry method

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -7,24 +7,24 @@ RSpec.describe AddressBook do
       expect(book).to respond_to(:entries)
     end
 
-  it "initializes entries as an array" do
-    book = AddressBook.new
-    expect(book.entries).to be_an(Array)
-  end
+    it "initializes entries as an array" do
+      book = AddressBook.new
+      expect(book.entries).to be_an(Array)
+    end
 
-  it "initializes entries as empty" do
-    book = AddressBook.new
-    expect(book.entries.size).to eq(0)
-  end
+    it "initializes entries as empty" do
+      book = AddressBook.new
+      expect(book.entries.size).to eq(0)
+    end
  end
 
  describe "#add_entry" do
-   it "adds only one entry to the address book" do
-    book = AddressBook.new
-    book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    it "adds only one entry to the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
-    expect(book.entries.size).to eq(1)
-   end
+      expect(book.entries.size).to eq(1)
+    end
 
    it "adds the correct information to entries" do
      book = AddressBook.new
@@ -34,6 +34,16 @@ RSpec.describe AddressBook do
      expect(new_entry.name).to eq('Ada Lovelace')
      expect(new_entry.phone_number).to eq('010.012.1815')
      expect(new_entry.email).to eq('augusta.king@lovelace.com')
+   end
+ end
+
+ describe "#remove_entry" do
+   it "removes the selected entry" do
+     book = AddressBook.new
+     book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+     book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+     expect(book.entries.size).to eq(0)
    end
  end
 end
