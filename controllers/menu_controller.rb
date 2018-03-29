8dabaffd29e8d4df0c4ea5b65f444c4ba6ccb59c
Linda <oneill.linda21@gmail.com>
2018-03-28 19:44:43 -0500

    add demolish_all

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,7 +15,8 @@ class MenuController
     puts "3 - Search for an entry"
     puts "4 - Import entries from a CSV"
     puts "5 - View entry number n"
-    puts "6 - Exit"
+    puts "6 - Delete all entries"
+    puts "7 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -42,6 +43,10 @@ class MenuController
       view_entry
       main_menu
     when 6
+      system "clear"
+      demolish_all
+      main_menu
+    when 7
       puts "Good-bye!"
 
       exit (0)
@@ -138,6 +143,10 @@ end
 def delete_entry(entry)
   address_book.entries.delete(entry)
   puts "#{entry.name} has been deleted"
+end
+
+def demolish_all
+  @entries = []
 end
 
 def edit_entry(entry)
